668651a3e9254fda4c9d5ea4878d5776bdcfc233
kevinbh168 <kevin.brena@tecsup.edu.pe>
2019-09-17 22:19:52 -0500

    lab05-terminado

---
+++ Lab05/Lab05/Button.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab05
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Button : ContentPage
+    {
+        public Button()
+        {
+            InitializeComponent();
+        }
+
+        public string Text { get; internal set; }
+        public LayoutOptions VerticalOptions { get; internal set; }
+        public LayoutOptions HorizontalOptions { get; internal set; }
+        public Func<object, object, Task<bool>> Clicked { get; internal set; }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
